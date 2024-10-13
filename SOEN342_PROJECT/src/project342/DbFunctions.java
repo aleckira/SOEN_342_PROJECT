08ac7bdf6ec35c69eb010d5dbc37e774206b1e79
Sevag <sevag.merdkhanian@gmail.com>
2024-10-13 15:38:16 -0400

    some changes

--- SOEN342_PROJECT/src/project342/DbFunctions.java
+++ SOEN342_PROJECT/src/project342/DbFunctions.java
@@ -2,19 +2,22 @@ package SOEN342_PROJECT.src.project342;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbFunctions {
     public Connection connectToDb() {
         Connection conn = null;
+        String url = "jdbc:postgresql://localhost:5432/SOEN342-Project";
+        String user = "postgres";
+        String password = "DataBahel420!";
         try {
-            Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" +  "SOEN342-Project", "postgres", "DataBahel420!");
+            conn = DriverManager.getConnection(url, user, password);
             if (conn != null) {
                 System.out.println("Connected to PostgreSQL database");
             } else {
                 System.out.println("Failed to connect to PostgreSQL database");
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println(e);
         }
         return conn;
--- module-info.java
+++ module-info.java
@@ -7,5 +7,4 @@
 module SOEN342_PROJECT {
 	requires java.desktop;
 	requires java.sql;
-	requires org.postgresql.jdbc;
 }
